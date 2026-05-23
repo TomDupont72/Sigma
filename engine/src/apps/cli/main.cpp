c4fce15cb35f8ee4fa6ee0b8cdb922d3e870f750
Dupont Tom <tomdupont72@gmail.com>
2026-05-24 01:17:28 +0200

    Refacto main

--- engine/src/apps/cli/main.cpp
+++ engine/src/apps/cli/main.cpp
@@ -10,25 +10,25 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-    string input = "((x*x)^2*(x^3)/(x*x*x))^2/(x*(x^2)^5)+(2*x+3*x-x)+(y*y)^3/(y^2*y)-(4*a-2*a+a)+(z^2)^0+(b^1)^2";
+    string command = argv[1];
+    string expression = argv[2];
 
-    vector<Token> res = lexer(input);
-
+    vector<Token> res = lexer(expression);
     Parser parser(res);
-
     Node * tree = parser.parseExpression();
 
-    printTree(tree);
+    if (command == "display")
+    {
+        cout << displayExpression(tree, 0);
+    }
 
-    cout << displayExpression(tree, 0) + '\n';
-
-    tree = normalize(tree);
-
-    printTree(tree);
-
-    cout << displayExpression(tree, 0);
+    if (command == "simplify")
+    {
+        tree = simplify(tree);
+        cout << displayExpression(tree, 0);
+    }
 
     return 0;
-};
+}
